501433cf1e5109fd8a6d647d560c682ee629a677
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2022-11-25 14:34:16 -0300

    Update routes API + CRUD

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 import harperdb
 
-HARPERDB_PASSWORD = 'YOUR DB PASSWORD'
-HARPERDB_URL = "YOUR DB URL"
-HARPERDB_USERNAME = "YOUR DB USERNAME"
+HARPERDB_PASSWORD = '@0519Dieferson'
+HARPERDB_URL = "https://notesapp-sigmadev.harperdbcloud.com"
+HARPERDB_USERNAME = "notesapp"
 db = harperdb.HarperDB(
     url=HARPERDB_URL,
     username=HARPERDB_USERNAME,
--- main.py
+++ main.py
@@ -1,8 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+
 from database import db
 
 app = FastAPI()
 
+
 @app.get("/")
 def get_notes():
     return "Aqui vai ter umas notas legais"
+
+    notes = db.search_by_value('notesapp', 'notes', 'id', '*',
+                               get_attributes=['*'])
+    return notes
+
+
+@app.post("/notes")
+def addNotes(data=Body()):
+    db.insert('notesapp', 'notes', [{'body': data['body']}])
+
+    notes = db.search_by_value('notesapp', 'notes', 'id', '*',
+                               get_attributes=['*'])
+
+    return notes
+
+
+@app.get("/notes/{pk}")
+def getNotes(pk: str):
+    notes = db.search_by_hash('notesapp', 'notes', [pk],
+                              get_attributes=['*'])
+    return notes[0]
+
+
+app.get("/notes/{id}")
+def updateNotes(id: str, data=Body()):
+    db.update('notesapp', 'notes', [{"id": id, "body": data["body"]}])
+
+    notes = db.search_by_value('notesapp', 'notes', 'id', '*',
+                               get_attributes=['*'])
+    return notes
+
+
+@app.delete("/notes/{id}")
+def deleteNote(id: str):
+    db.delete('notesapp', 'notes', '[id]')
+
+    notes = db.search_by_value('notesapp', 'notes', 'id', '*',
+                               get_attributes=['*'])
+    return notes
+
