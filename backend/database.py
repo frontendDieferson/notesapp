8d51b39f926f92b7f46cc84f948db59231e678c3
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2022-12-01 11:03:07 -0300

    Criação de diretório Backend

---
+++ backend/database.py
@@ -0,0 +1,9 @@
+import harperdb
+
+from backend.secret import HARPERDB_PASSWORD, HARPERDB_URL, HARPERDB_USERNAME
+
+db = harperdb.HarperDB(
+    url=HARPERDB_URL,
+    username=HARPERDB_USERNAME,
+    password=HARPERDB_PASSWORD
+)
