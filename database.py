d1aaa7f404aa157046452f7318c71bf69bfc7834
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2022-12-01 11:03:57 -0300

    Criação Backend

--- database.py
+++
@@ -1,9 +0,0 @@
-import harperdb
-
-from secret import HARPERDB_PASSWORD, HARPERDB_URL, HARPERDB_USERNAME
-
-db = harperdb.HarperDB(
-    url=HARPERDB_URL,
-    username=HARPERDB_USERNAME,
-    password=HARPERDB_PASSWORD
-)
